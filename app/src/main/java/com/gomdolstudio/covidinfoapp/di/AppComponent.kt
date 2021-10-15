8c896e999d1fec31d40a0e724257a7d2299ff2bd
jjeewon <wldnjs594@gmail.com>
2021-10-15 21:47:35 +0900

    앱컴포넌트에 스코프 모듈(액티비티, 애플리케이션) 추가

--- app/src/main/java/com/gomdolstudio/covidinfoapp/di/AppComponent.kt
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/AppComponent.kt
@@ -1,13 +1,15 @@
 package com.gomdolstudio.covidinfoapp.di
 
 import com.gomdolstudio.covidinfoapp.App
+import com.gomdolstudio.covidinfoapp.di.module.ActivityModule
+import com.gomdolstudio.covidinfoapp.di.module.AppModule
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidSupportInjectionModule::class])
+@Component(modules = [(AndroidSupportInjectionModule::class), (ActivityModule::class), (AppModule::class)])
 interface AppComponent : AndroidInjector<App> {
     @Component.Factory
     abstract class Factory: AndroidInjector.Factory<App>{}
