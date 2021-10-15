370c4df8f4da76310e85e29db74f4c1ddef4d593
jjeewon <wldnjs594@gmail.com>
2021-10-15 22:51:49 +0900

    ViewModel을 위한 사용자 정의키 정의

---
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/ViewModelKey.kt
@@ -0,0 +1,12 @@
+package com.gomdolstudio.covidinfoapp.di
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
+
