d86ffb1a160257fd5aa8d6e69f66d292036a830d
jjeewon <wldnjs594@gmail.com>
2021-10-15 16:43:00 +0900

    앱 컴포넌트 생성

---
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/App.kt
@@ -0,0 +1,8 @@
+package com.gomdolstudio.covidinfoapp
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+open class App {
+
+}
---
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/AppComponent.kt
@@ -0,0 +1,11 @@
+package com.gomdolstudio.covidinfoapp.di
+
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent{
+}
