5938c2d8324ce1f9427432573c79f7f6147fd1b2
jjeewon <wldnjs594@gmail.com>
2021-10-18 21:10:43 +0900

    액티비티 모듈에 Main 액티비티 추가

--- app/src/main/java/com/gomdolstudio/covidinfoapp/di/module/ActivityModule.kt
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/module/ActivityModule.kt
@@ -1,9 +1,14 @@
 package com.gomdolstudio.covidinfoapp.di.module
 
+import com.gomdolstudio.covidinfoapp.MainActivity
+import com.gomdolstudio.musicapp_assistedinjection.di.scope.ActivityScope
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityModule {
-
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [(MainModule::class)])
+    abstract fun getMainActivity(): MainActivity
 }
 
