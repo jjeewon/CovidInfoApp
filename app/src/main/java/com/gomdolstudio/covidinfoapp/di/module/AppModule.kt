3bb37e76b55358a5357a2bc9d458bdf5faaf4e1d
jjeewon <wldnjs594@gmail.com>
2021-10-18 21:09:44 +0900

    앱모듈에 Retrofit 객체 추가

--- app/src/main/java/com/gomdolstudio/covidinfoapp/di/module/AppModule.kt
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/module/AppModule.kt
@@ -4,13 +4,28 @@ import android.app.Application
 import android.content.Context
 import com.gomdolstudio.covidinfoapp.App
 import com.gomdolstudio.covidinfoapp.ApplicationContext
+import com.gomdolstudio.covidinfoapp.di.RetrofitModule
 import com.gomdolstudio.covidinfoapp.di.ViewModelModule
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@Module(includes = [(ViewModelModule::class)])
+@Module(includes = [(ViewModelModule::class),(RetrofitModule::class)])
 class AppModule {
+
+    @Singleton
+    @Provides
+    internal fun provideVaccinationRetrofitService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.odcloud.kr/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
     @Provides
     @Singleton
     fun provideApp(app: App): Application {
