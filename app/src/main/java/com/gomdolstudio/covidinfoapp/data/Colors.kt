d653826364b010f880538174d6f520a108acf867
jjeewon <wldnjs594@gmail.com>
2021-10-19 00:21:28 +0900

    Colors 오브젝트 정의

---
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/data/Colors.kt
@@ -0,0 +1,16 @@
+package com.gomdolstudio.covidinfoapp.data
+
+import com.gomdolstudio.covidinfoapp.R
+
+object Colors {
+    val colors: IntArray = intArrayOf(
+        R.color.gradient_1,
+        R.color.gradient_2,
+        R.color.gradient_3,
+        R.color.gradient_4,
+        R.color.gradient_5,
+        R.color.gradient_6,
+        R.color.gradient_7,
+        R.color.gradient_8)
+
+}
