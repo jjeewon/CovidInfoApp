70ec2d4e8b7c741160fb17eb2a42b77b59308807
jjeewon <wldnjs594@gmail.com>
2021-10-19 22:07:15 +0900

    의존성을 주입받는 retrofit에 각각에 맞는 한정자 지정

--- app/src/main/java/com/gomdolstudio/covidinfoapp/di/RetrofitModule.kt
+++ app/src/main/java/com/gomdolstudio/covidinfoapp/di/RetrofitModule.kt
@@ -1,25 +1,33 @@
 package com.gomdolstudio.covidinfoapp.di
 
 import com.gomdolstudio.covidinfoapp.data.service.CasesLocationService
+import com.gomdolstudio.covidinfoapp.data.service.NewsService
 import com.gomdolstudio.covidinfoapp.data.service.VaccinationCenterService
 
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import retrofit2.Retrofit
+import javax.inject.Named
 
 @Module
 class RetrofitModule {
     @Provides
     @Reusable
-    fun provideVaccinationLocationRetrofitService(retrofit: Retrofit): CasesLocationService {
+    fun provideVaccinationLocationRetrofitService(@Named("vaccine")retrofit: Retrofit): CasesLocationService {
         return retrofit.create(CasesLocationService::class.java)
     }
 
     @Provides
     @Reusable
-    fun provideVaccinationCenterRetrofitService(retrofit: Retrofit): VaccinationCenterService{
+    fun provideVaccinationCenterRetrofitService(@Named("vaccine")retrofit: Retrofit): VaccinationCenterService{
         return retrofit.create(VaccinationCenterService::class.java)
+    }
+
+    @Provides
+    @Reusable
+    fun provideNewsRetrofitService(@Named("news")retrofit: Retrofit): NewsService{
+        return retrofit.create(NewsService::class.java)
     }
 }
 
