522996ad724cc111e1c97727607451e62fbfcb1c
jjeewon <wldnjs594@gmail.com>
2021-10-19 22:04:10 +0900

    뉴스데이터 유닛테스트
    네이버 검색 API에서 Retrofit을 통해 데이터가 잘 받아와지는지 테스트

--- app/src/test/java/com/gomdolstudio/covidinfoapp/DataTest.kt
+++ app/src/test/java/com/gomdolstudio/covidinfoapp/DataTest.kt
@@ -1,7 +1,8 @@
 package com.gomdolstudio.covidinfoapp
 
 import com.gomdolstudio.covidinfoapp.data.service.VaccinationCenterService
-import com.gomdolstudio.covidinfoapp.data.service.VaccinationLocationService
+import com.gomdolstudio.covidinfoapp.data.service.CasesLocationService
+import com.gomdolstudio.covidinfoapp.data.service.NewsService
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import org.junit.Before
 import org.junit.Test
@@ -10,7 +11,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class DataTest {
     lateinit var vaccinationCenterService: VaccinationCenterService
-    lateinit var vaccinationLocationService: VaccinationLocationService
+    lateinit var casesLocationService: CasesLocationService
+    lateinit var newsService: NewsService
     @Before
     fun 데이터서비스세팅(){
         vaccinationCenterService = Retrofit.Builder()
@@ -19,11 +21,17 @@ class DataTest {
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build().create(VaccinationCenterService::class.java)
 
-        vaccinationLocationService = Retrofit.Builder()
+        casesLocationService = Retrofit.Builder()
             .baseUrl("https://api.odcloud.kr/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build().create(VaccinationLocationService::class.java)
+            .build().create(CasesLocationService::class.java)
+
+        newsService = Retrofit.Builder()
+                .baseUrl("https://openapi.naver.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build().create(NewsService::class.java)
     }
     @Test
     fun 백신센터데이터테스트(){
@@ -34,7 +42,13 @@ class DataTest {
 
     @Test
     fun 백신현황데이터테스트(){
-        vaccinationLocationService.getVaccinationLocation("1","10","qMYuD3Bao+aVVtqIcLH1sqz80i+otqoifcU1C7frDT+SVjQD9FXw8uT1CqYcME+q+A8YKoiZns1FXGGgJVwVzg==","2021-10-11 00:00:00")
+        casesLocationService.getCasesLocation("1","10","qMYuD3Bao+aVVtqIcLH1sqz80i+otqoifcU1C7frDT+SVjQD9FXw8uT1CqYcME+q+A8YKoiZns1FXGGgJVwVzg==","2021-10-11 00:00:00")
             .subscribe(System.out::println)
+    }
+
+    @Test
+    fun 뉴스데이터테스트(){
+        newsService.getNews("코로나백신접종",10,1,"sim")
+                .subscribe(System.out::println)
     }
 }
